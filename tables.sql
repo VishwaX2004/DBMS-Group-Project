db3dc71f93f307c48eedc42d03152bec3a72aad8
pasindumethmadu2004-rgb <pasindumethmadu2004@gmail.com>
2026-05-04 19:53:00 +0530

    Update tables.sql
    insert data to the tables

--- tables.sql
+++ tables.sql
@@ -43,6 +43,7 @@ CREATE TABLE `admin` (
 
 LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES ('ADM01','A001','University','Admin','0112222333','admin@ruhuna.ac.lk','2024-01-01');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -74,6 +75,7 @@ CREATE TABLE `dean` (
 
 LOCK TABLES `dean` WRITE;
 /*!40000 ALTER TABLE `dean` DISABLE KEYS */;
+INSERT INTO `dean` VALUES ('DEN01','D001','Prof. Sunil Perera','1970-05-15','2020-01-10','dean@ruhuna.ac.lk','0114444555');
 /*!40000 ALTER TABLE `dean` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -101,6 +103,31 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('A001','admin','12345','admin@ruhuna.ac.lk','0112222333'),
+('D001','dean','12345','dean@ruhuna.ac.lk','0114444555'),
+('L001','lecturer','12345','lec1@mail.com','0770000101'),
+('L002','lecturer','12345','lec2@mail.com','0770000102'),
+('L003','lecturer','12345','lec3@mail.com','0770000103'),
+('L004','lecturer','12345','lec4@mail.com','0770000104'),
+('L005','lecturer','12345','lec5@mail.com','0770000105'),
+('S001','student','12345','stu01@mail.com','0750000201'),
+('S002','student','12345','stu02@mail.com','0750000202'),
+('S003','student','12345','stu03@mail.com','0750000203'),
+('S004','student','12345','stu04@mail.com','0750000204'),
+('S005','student','12345','stu05@mail.com','0750000205'),
+('S006','student','12345','stu06@mail.com','0750000206'),
+('S007','student','12345','stu07@mail.com','0750000207'),
+('S008','student','12345','stu08@mail.com','0750000208'),
+('S009','student','12345','stu09@mail.com','0750000209'),
+('S010','student','12345','stu10@mail.com','0750000210'),
+('S011','student','12345','stu11@mail.com','0750000211'),
+('S012','student','12345','stu12@mail.com','0750000212'),
+('S013','student','12345','stu13@mail.com','0750000213'),
+('S014','student','12345','stu14@mail.com','0750000214'),
+('S015','student','12345','stu15@mail.com','0750000215'),
+('T001','Technical_officer','12345','to1@mail.com','0710000001'),
+('T002','Technical_officer','12345','to2@mail.com','0710000002'),
+('T003','Technical_officer','12345','to3@mail.com','0710000003');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -113,4 +140,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-04 11:34:05
+-- Dump completed on 2026-05-04 19:47:57
